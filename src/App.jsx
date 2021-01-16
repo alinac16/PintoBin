198eb6bf2d38f71609d47a26c4c452aa3ecb5d02
Alina Chalanuchpong <alina.c@alumni.ubc.ca>
2021-01-16 14:06:08 -0500

    implement tabs

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Home, MapMarkedAlt, Recycle } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function CenteredTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab icon={<Home />} />
+        <Tab icon={<MapMarkedAlt />} />
+        <Tab icon={<Recycle />} />
+      </Tabs>
+    </Paper>
+  );
+}
