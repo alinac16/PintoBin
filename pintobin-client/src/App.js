3b768449ed363f06753f7f7d22e97607cfb8a596
Alina Chalanuchpong <alina.c@alumni.ubc.ca>
2021-01-16 01:08:49 -0500

    initial client set up

---
+++ pintobin-client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Pin to Bin</h1>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ pintobin-client/src/pages/landing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function landing() {
+  return <div></div>;
+}
+
+export default landing;
